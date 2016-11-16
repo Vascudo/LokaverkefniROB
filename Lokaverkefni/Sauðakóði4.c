e7fac806390ba8735a1381f5a4833f15d48ffb61
Vascudo <Margeir26@gmail.com>
2016-11-16 10:07:34 +0000

    Add files via upload

---
+++ Lokaverkefni/Sauðakóði4.c
@@ -0,0 +1,9 @@
+
+task main()
+{
+while if there is light then the robot will be on.
+	if the sonar sensor value is over 20 or -1 the robot will go forward.
+	else if the sonar sensor value goes under 20 the robot will stop then turn until it goes back over 20.
+
+
+}
