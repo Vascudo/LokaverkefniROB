c8a48bcd274ae62ee2c22d416380520d5d06f1ae
Vascudo <Margeir26@gmail.com>
2016-11-30 09:53:48 +0000

    Add files via upload

---
+++ LokaSauðakóði.c
@@ -0,0 +1,14 @@
+void Stopmotors //*Functions for the main task and headers to call upon*//
+void clawOpen //*Opens claw*//
+void clawClose//*Closes claw*//
+void armUp//*Moves arm up*//
+void armDown//*moves arm down*//
+void turn180//*Turns the robot 180 degrees*//
+#include header  verk 1-5//*This includes the headers so the main task can call upon their functions.*//
+task main()
+{
+while makes the program run indefinetly until you turn off the robot
+	{
+		a different if function is called upon with a diffirent button on the remote, each calling upon a function in a header
+	}
+}
